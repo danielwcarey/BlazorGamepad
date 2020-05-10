e7c4b2d2b90ef185a528091f4a6a81730b75ab25
Daniel Carey <whawke8@hotmail.com>
2020-05-09 19:52:32 -0500

    Added logic to determin what changed between previous settings. Added UX for debugging the gamepad control.

---
+++ src/DanielCarey.Blazor.Controls/ClientGamepadUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DanielCarey.Blazor.Controls {
+    public class ClientGamepadUpdateArgs {
+        public ClientGamepad? PreviousValue { get; set; } = null;
+        public ClientGamepad? NewValue { get; set; } = null;
+        public List<int> ChangedAxesIndexes { get; } = new List<int>();
+        public List<int> ChangedButtonIndexes { get; } = new List<int>();
+    }
+
+}
